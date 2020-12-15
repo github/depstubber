de0d8ea1ab164ad1de5264d1adc1798c59ff6163
Slavomir <gagliardetto@users.noreply.github.com>
2020-12-15 01:09:22 +0100

    Fix comment inside printGoGenerateComments

--- auto-detect.go
+++ auto-detect.go
@@ -245,8 +245,8 @@ func printGoGenerateComments(pathToTypeNames map[string][]string, pathToFuncAndV
 		fmt.Println(comment)
 	}
 
-	// If `depstubber -write_module_txt` is not extecuted, then
-	// you'll encunter an `go: inconsistent vendoring in ...` error;
-	// Printing this as a reminder.
+	// If `depstubber -write_module_txt` is not executed, then
+	// you'll encunter a `go: inconsistent vendoring in ...` error;
+	// Printing this as a reminder can save time for a few people.
 	fmt.Println("//go:generate depstubber -write_module_txt")
 }
